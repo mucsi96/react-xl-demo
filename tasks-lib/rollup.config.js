af3e948e3181a2e1342628d42b5c8fe3eb61460b
Igor Mucsicska <mucsi_96@yahoo.com>
2020-11-08 22:17:46 +0100

    Finalize rollup setup

--- tasks-lib/rollup.config.js
+++ tasks-lib/rollup.config.js
@@ -1,6 +1,7 @@
+import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
+import url from "@rollup/plugin-url";
 import svgr from "@svgr/rollup";
 import { dirname } from "path";
 import external from "rollup-plugin-peer-deps-external";
@@ -8,6 +9,7 @@ import postcss from "rollup-plugin-postcss";
 import visualizer from "rollup-plugin-visualizer";
 import packageJson from "./package.json";
 
+const extensions = [".js", ".jsx", ".ts", ".tsx"];
 const config = {
   input: "./src/index.tsx",
   output: [
@@ -26,9 +28,16 @@ const config = {
   plugins: [
     external(),
     postcss(),
-    typescript(),
-    resolve(),
+    resolve({
+      extensions,
+    }),
+    babel({
+      babelHelpers: "runtime",
+      extensions,
+      include: ["src/**"],
+    }),
     commonjs(),
+    url(),
     svgr(),
     visualizer(),
   ],
